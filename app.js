ca7f81aac436e394b1e8526589a67b84d38b7333
coolshano <37379521+coolshano@users.noreply.github.com>
2019-02-07 00:02:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const fs = require("fs");
-const hostname = '127.0.0.1';
+const hostname = 'ec2-13-59-229-184.us-east-2.compute.amazonaws.com';
 const port = 3000;
 
 fs.readFile('index.html',(err, html)  => {
